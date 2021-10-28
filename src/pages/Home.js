a7e550fe89484e6a2cdfcb61e9d73899f9209eef
moeini-s <johndoe@example.com>
2021-10-28 21:46:11 +0330

    commit2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ const Home = () => {
     return (  
         <div className="home">
             <div className="home__header">
-                <img src={"../img/job.png"}/>
+                <img src={"./img/job.png"}/>
                 <div>Find a job, Today!</div>
             </div>
         </div>
